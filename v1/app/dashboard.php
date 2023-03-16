6d85c4259d7f5c18c9fb62d0e50a23c570b3b724
Geperson Mamalias <kuyagepofficial@gmail.com>
2023-03-16 17:05:18 +0800

    Revert "updates"
    This reverts commit 28c5f3b965274181dade13a224c8789afa6b247e.

--- v1/app/dashboard.php
+++ v1/app/dashboard.php
@@ -33,7 +33,7 @@
                                 <h3>
 
                                     <?php
-                                            $sql = "SELECT * from inv_ict";
+                                            $sql = "SELECT * from pmr_table";
                                             if ($result = mysqli_query($con, $sql)) {
                                                 // Return the number of rows in result set
                                                 $rowcount = mysqli_num_rows( $result );
@@ -114,7 +114,7 @@
                             <div class="inner">
                                 <h3>
                                     <?php
-                                            $sql = "SELECT * from inv_ict";
+                                            $sql = "SELECT * from pmr_table";
                                             $amount = 0;
                                             if ($result = mysqli_query($con, $sql)) {
                                                 // Return the number of rows in result set
--- v1/app/ict-edit.php
+++ v1/app/ict-edit.php
@@ -2,13 +2,11 @@
 
     if(isset($_GET['id'])){
         $idx = $_GET['id'];
-        $query = mysqli_query($con,"SELECT `inv_ict`.`inv_id`,`inv_ict`.`inv_no`, `inv_ict`.`employee_id`, 
-        `inv_ict`.`item_name`, `inv_ict`.`specs`, `inv_ict`.`amount`, `inv_ict`.`serial_no`, `inv_ict`.`date_acquired`, `inv_ict`.`category_id`, 
-        `inv_ict`.`date_inspection`, `inv_ict`.`inspected_by` FROM `inv_ict` left JOIN `employee` ON `employee`.`employee_id`=`inv_ict`.`employee_id` 
-        INNER JOIN `office` ON `employee`.`office_id`=`office`.`office_id` 
-        INNER JOIN category ON `category`.`category_id`=`inv_ict`.`category_id` WHERE inv_id=$idx");
-    
-        include('action/admin/edit-ict.php');
+        $query = mysqli_query($con,"SELECT pmr_table.pmr_id, pmr_table.pr_no, pmr_table.po_no,  pmr_table.date, pmr_table.pcategory_id, pmr_table.particulars, 
+        pmr_table.amount, pmr_table.src_fund, pmr_table.employee_id, pmr_table.pstatus_id, pmr_table.remarks FROM pmr_table LEFT JOIN employee ON 
+        pmr_table.employee_id=employee.employee_id INNER JOIN office ON employee.office_id=office.office_id INNER JOIN procurement_category ON 
+        procurement_category.pcategory_id=pmr_table.pcategory_id  INNER JOIN po_status ON po_status.pstatus_id=pmr_table.pstatus_id WHERE pmr_id='$idx'");
+    include('action/admin/edit-purchase.php');
     }
     
 ?>
