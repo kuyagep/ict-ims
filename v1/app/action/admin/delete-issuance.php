0361e1938b2409a0f571f2a51ef88c1de445c188
Geperson Mamalias <kuyagepofficial@gmail.com>
2023-03-17 14:44:35 +0800

    update

--- v1/app/action/admin/delete-issuance.php
+++ v1/app/action/admin/delete-issuance.php
@@ -1,11 +1,11 @@
-<?php
-    include('../../../conf/config.php');
+<?php 
+   include('../../../conf/config.php');
 
-    if(isset($_GET['id'])){
-        $id=$_GET['id'];
+   if(isset($_GET['id'])){
+       $id=$_GET['id'];
 
-        $query=mysqli_query($con,"DELETE FROM issuance WHERE issued_id='".$id."'");
-        header("location: ../../issued-items");
-    }
-
+       //$query=mysqli_query($con,"DELETE FROM pmr_table WHERE pmr_id='".$id."'");
+       $query=mysqli_query($con,"UPDATE `inv_ict` SET `deleted`= 0  WHERE inv_id='".$id."'");
+       header("location: ../../index.php?page=inventory&d-1");
+   }
 ?>
---
+++ v1/app/action/admin/update-ict.php
@@ -0,0 +1,39 @@
+<?php 
+include('../../../conf/config.php');
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $id = validate($_POST["id"]);
+    $inv_no = validate($_POST["inv_no"]);
+    $end_user = validate($_POST["end_user"]);
+    $item_name = validate($_POST["item_name"]);
+    $specs = validate($_POST["specs"]);
+    $amount = validate($_POST["amount"]);
+    $serial_no = validate($_POST["serial_no"]);
+    $date_acquired = validate($_POST["date_acquired"]);
+    $category = validate($_POST["category"]);
+    $date_inspection = validate($_POST["date_inspection"]);
+    $inspected_by = validate($_POST["inspected_by"]);
+
+    $query=mysqli_query($con,"UPDATE `inv_ict` SET 
+    `inv_id`='".$id."',
+    `employee_id`='".$end_user."',
+    `item_name`='".$item_name."',
+    `specs`='".$specs."',
+    `amount`='".$amount."',
+    `serial_no`='".$serial_no."',
+    `date_acquired`='".$date_acquired."',
+    `category_id`='".$category."',
+    `date_inspection`='".$date_inspection."', 
+    `inspected_by`='".$inspected_by."' 
+    WHERE  `inv_id`='".$id."'");
+    header("Location: ../../index.php?page=ict-edit&id=".$id);
+    }
+
+    function validate($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+?>
