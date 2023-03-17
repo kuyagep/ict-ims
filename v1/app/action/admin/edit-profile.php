65a24d43f24477664af21d361ccdc58d63f1c3bb
Geperson Mamalias <kuyagepofficial@gmail.com>
2023-03-17 15:40:47 +0800

    Update edit-profile.php

--- v1/app/action/admin/edit-profile.php
+++ v1/app/action/admin/edit-profile.php
@@ -64,7 +64,7 @@ $view = mysqli_fetch_array($query);
                                     }
                                 ?></p>
 
-                            <ul class="list-group list-group-unbordered mb-3">
+                            <!-- <ul class="list-group list-group-unbordered mb-3">
                                 <li class="list-group-item">
                                     <b>Followers</b> <a class="float-right">1,322</a>
                                 </li>
@@ -74,7 +74,7 @@ $view = mysqli_fetch_array($query);
                                 <li class="list-group-item">
                                     <b>Friends</b> <a class="float-right">13,287</a>
                                 </li>
-                            </ul>
+                            </ul> -->
 
                             <button href="#" name="updateEmployee" class="btn btn-primary btn-block"><b>Update
                                     Profile</b></button>
