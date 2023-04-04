4037e70e0cc345bb432f92936dc67493dbb34b26
Geperson Mamalias <kuyagepofficial@gmail.com>
2023-04-04 13:54:52 +0800

    Update dashboard.php

--- v1/app/dashboard.php
+++ v1/app/dashboard.php
@@ -133,7 +133,7 @@
                     <div class="col-md-4">
 
                         <div class="info-box mb-3 bg-warning">
-                            <span class="info-box-icon"><i class="fas fa-tag"></i></span>
+                            <span class="info-box-icon"><i class="fas fa-solid fa-laptop"></i></span>
                             <div class="info-box-content">
                                 <span class="info-box-text">Laptop</span>
                                 <span class="info-box-number">5,200</span>
@@ -142,7 +142,7 @@
                         </div>
 
                         <div class="info-box mb-3 bg-success">
-                            <span class="info-box-icon"><i class="far fa-heart"></i></span>
+                            <span class="info-box-icon"><i class="fas fa-solid fa-tablet"></i></span>
                             <div class="info-box-content">
                                 <span class="info-box-text">Tablet</span>
                                 <span class="info-box-number">92,050</span>
@@ -151,7 +151,7 @@
                         </div>
 
                         <div class="info-box mb-3 bg-danger">
-                            <span class="info-box-icon"><i class="fas fa-cloud-download-alt"></i></span>
+                            <span class="info-box-icon"><i class="fas fa-sharp fa-solid fa-desktop"></i></span>
                             <div class="info-box-content">
                                 <span class="info-box-text">PC</span>
                                 <span class="info-box-number">114,381</span>
@@ -160,7 +160,7 @@
                         </div>
 
                         <div class="info-box mb-3 bg-info">
-                            <span class="info-box-icon"><i class="far fa-comment"></i></span>
+                            <span class="info-box-icon"><i class="fas fa-solid fa-clipboard"></i></span>
                             <div class="info-box-content">
                                 <span class="info-box-text">Others</span>
                                 <span class="info-box-number">163,921</span>
@@ -168,8 +168,86 @@
 
                         </div>
 
-                        
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card">
+<div class="card-header">
+<h3 class="card-title">Recently Added Inventory</h3>
+<div class="card-tools">
+<button type="button" class="btn btn-tool" data-card-widget="collapse">
+<i class="fas fa-minus"></i>
+</button>
+<button type="button" class="btn btn-tool" data-card-widget="remove">
+<i class="fas fa-times"></i>
+</button>
+</div>
+</div>
 
+<div class="card-body p-0">
+<ul class="products-list product-list-in-card pl-2 pr-2">
+<li class="item">
+<div class="product-img">
+<img src="dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
+</div>
+<div class="product-info">
+<a href="javascript:void(0)" class="product-title">Samsung TV
+<span class="badge badge-warning float-right">$1800</span></a>
+<span class="product-description">
+Samsung 32" 1080p 60Hz LED Smart HDTV.
+</span>
+</div>
+</li>
+
+<li class="item">
+<div class="product-img">
+<img src="dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
+</div>
+<div class="product-info">
+<a href="javascript:void(0)" class="product-title">Bicycle
+<span class="badge badge-info float-right">$700</span></a>
+<span class="product-description">
+26" Mongoose Dolomite Men's 7-speed, Navy Blue.
+</span>
+</div>
+</li>
+
+<li class="item">
+<div class="product-img">
+<img src="dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
+</div>
+<div class="product-info">
+<a href="javascript:void(0)" class="product-title">
+Xbox One <span class="badge badge-danger float-right">
+$350
+</span>
+</a>
+<span class="product-description">
+Xbox One Console Bundle with Halo Master Chief Collection.
+</span>
+</div>
+</li>
+
+<li class="item">
+<div class="product-img">
+<img src="dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
+</div>
+<div class="product-info">
+<a href="javascript:void(0)" class="product-title">PlayStation 4
+<span class="badge badge-success float-right">$399</span></a>
+<span class="product-description">
+PlayStation 4 500GB Console (PS4)
+</span>
+</div>
+</li>
+
+</ul>
+</div>
+
+<div class="card-footer text-center">
+<a href="javascript:void(0)" class="uppercase">View All Products</a>
+</div>
+
+</div>
                     </div>
                 </div>
             </div>
