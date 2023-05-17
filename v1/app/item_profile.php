f9f4f2381d4a771299b4e7907e98cefaa408b6c3
wing105 <132327969+wing105@users.noreply.github.com>
2023-05-17 22:34:31 +0800

    Update item_profile.php

--- v1/app/item_profile.php
+++ v1/app/item_profile.php
@@ -108,81 +108,8 @@ $view = mysqli_fetch_array($query);
             <div class="col-sm-12">
                 <div class="card card-outline card-red">
                     <div class="card-header">
-                        <h3 class="card-title"> <i class="fa-solid fa-pen-to-square mr-2"></i> <STRONG>List of Inventory</STRONG></h3>
-
-                        
+                        <h3 class="card-title"> <i class="fa-solid fa-pen-to-square mr-2"></i> <STRONG>ACTIONS:</STRONG></h3>
                     </div>
-                    <!-- /.card-header -->
-                    <div class="card-body">
-
-                        <table id="dataTable" class="table table-hover " width="100%"
-                            cellspacing="0">
-                            <thead class=" thead-dark">
-                            <tr>
-                                <!-- <th>Inventory No</th> -->
-                                <!-- <th>Office Name</th> -->
-                                <th>Item Name</th>
-                                <th>Specifications</th>
-                                <th>Unit Price</th>
-                                <th>Serial No.</th>
-                                <th>Date Acquired</th>
-                                <th>Item Category</th>
-                                <th>Inspected by</th>
-                                <!-- <th>Action</th> -->
-                            </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                $result = mysqli_query($con,"SELECT `inv_ict`.`inv_id`, `office`.`office_name`,`employee`.`employee_id`, `employee`.`firstname`, `employee`.`lastname`, 
-                                `inv_ict`.`item_name`, `inv_ict`.`specs`, `inv_ict`.`price`, `inv_ict`.`serial_no`, `inv_ict`.`date_acquired`, `category`.`category_name`, 
-                                `inv_ict`.`date_inspection`, `inv_ict`.`inspected_by`
-                                FROM `inv_ict` left JOIN `employee` ON `employee`.`employee_id`=`inv_ict`.`employee_id` INNER JOIN `office` ON `employee`.`office_id`=`office`.`office_id` 
-                                INNER JOIN category ON `category`.`category_id`=`inv_ict`.`category_id` WHERE `employee`.`employee_id` ='$idx'   ORDER BY `inv_ict`.`updated_at` DESC;");
-                                $count=1;
-                                $rowCount = mysqli_num_rows($result);
-                                if($rowCount > 0){
-                                    while($row = mysqli_fetch_assoc($result)){
-                                         $id=$row['inv_id'];
-                                         ?>
-                                <tr>
-                                    
-                                    <td><?php echo $row['item_name']; ?></td>
-                                    <td><?php echo $row['specs']; ?></td>
-                                    <td><?php 
-                                    echo number_format($row['price'], 2);
-                                    ?></td>
-                                    <td><?php echo $row['serial_no']; ?></td>
-
-                                    <td><?php echo $row['date_acquired']; ?></td>
-                                    <td>
-                                        
-                                        <span class="badge badge-primary"><?php echo $row['category_name']; ?></span>
-                                    </td>
-                                    <td><?php echo $row['inspected_by']; ?></td>
-
-                                    <!-- <td style='width: 100px;'>
-                                        <a href="index.php?page=pmr-edit&id=<?php echo $id; ?>" class="text-primary "
-                                            title="Edit">
-                                            <i class="fas fa-solid fa-pen"></i>
-                                        </a>
-
-                                        <a onclick="delete_item('<?php echo $id; ?>')" class="text-danger ml-2"
-                                            title="Delete">
-                                            <i class="fas fa-solid fa-trash"></i></a>
-                                    </td> -->
-
-                                </tr>
-                                <?php
-                                    $count++;
-                                }
-                                    
-                                }
-                                
-                            ?>
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- /.card-body -->
                 </div>
             </div>
         </div>
@@ -200,7 +127,7 @@ $view = mysqli_fetch_array($query);
 
 <!-- /.content-wrapper -->
 
-<script>
+<!-- <script>
 $('.file-upload').file_upload();
 
 function delete_employee(data_id) {
@@ -227,4 +154,4 @@ function delete_employee(data_id) {
 
     })
 }
-</script>
+</script> -->
