d179aa0c5f49acbe30ec571f5ca7e77730e38c1d
wing105 <132327969+wing105@users.noreply.github.com>
2023-05-22 13:49:20 +0800

    Revert "My Acccount Changes"
    This reverts commit a4cf0fe2ea66a1be2cf07204430ee26e02e0b5c5.

--- v1/app/action/admin/update-profile.php
+++ v1/app/action/admin/update-profile.php
@@ -3,21 +3,24 @@
 
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         $id=$_POST['id'];
+        $firstname=$_POST['firstname'];
+        $middlename=$_POST['middlename'];
+        $lastname=$_POST['lastname'];
         $username=$_POST['username'];
         $password=$_POST['change_password'];
         $password = password_hash($password, PASSWORD_DEFAULT);
         $contact=$_POST['contact'];
         $email=$_POST['email'];
         $position=$_POST['position'];
+        $office=$_POST['office'];
         //picture//
         $file_name = $_FILES['picture']['name'];
         
         $file_temp = $_FILES['picture']['tmp_name'];
         move_uploaded_file($file_temp, '../../dist/img/users/'.$file_name);
-        $query=mysqli_query($con,"UPDATE `employee` SET `username`='".$username."', `password`='".$password."',
-                         `emp_contact_no`='".$contact."',`emp_email_add`='".$email."',`position_id`='".$position."',
-                         `office_id`='".$office."', `picture`='".$file_name."' WHERE  `employee_id`='".$id."'");
-         header("Location: ../../profile");
+        $query=mysqli_query($con,"UPDATE `employee` SET `firstname`='".$firstname."',`middlename`='".$middlename."',`lastname`='".$lastname."', `username`='".$username."', `password`='".$password."',
+                         `emp_contact_no`='".$contact."',`emp_email_add`='".$email."',`position_id`='".$position."',`office_id`='".$office."',`role_id`='".$role."', `picture`='".$file_name."' WHERE  `employee_id`='".$id."'");
+         header("Location: ../../index.php?page=profile");
 
     }
 ?>
