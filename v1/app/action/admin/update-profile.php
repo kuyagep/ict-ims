3ce201764e235dc41e239dadb19128e7f49ba801
wing105 <132327969+wing105@users.noreply.github.com>
2023-05-22 14:15:25 +0800

    Update update-profile.php

--- v1/app/action/admin/update-profile.php
+++ v1/app/action/admin/update-profile.php
@@ -12,18 +12,11 @@
         
         $file_temp = $_FILES['picture']['tmp_name'];
         move_uploaded_file($file_temp, '../../dist/img/users/'.$file_name);
-<<<<<<< Updated upstream
-        $query=mysqli_query($con,"UPDATE `employee` SET `username`='".$username."', `password`='".$password."',
-                         `emp_contact_no`='".$contact."',`emp_email_add`='".$email."',`position_id`='".$position."',
-                         `office_id`='".$office."', `picture`='".$file_name."' WHERE  `employee_id`='".$id."'");
-         header("Location: ../../profile");
-=======
         $query=mysqli_query($con,"UPDATE `employee` SET `username`='".$username."',
                          `emp_contact_no`='".$contact."',`emp_email_add`='".$email."',
                          `position_id`='".$position."',`office_id`='".$office."',
                          `picture`='".$file_name."' WHERE  `employee_id`='".$id."'");
          header("Location: ../../index.php?page=profile");
->>>>>>> Stashed changes
 
     }
 ?>
