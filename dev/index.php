45d76359a36e8ae05aba4758d221f11fbc13e815
Geperson Mamalias <kuyagepofficial@gmail.com>
2023-07-04 14:55:19 +0800

    updates

---
+++ dev/index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- SEO Meta Tags -->
+    <meta name="description" content="Your description">
+    <meta name="author" content="Your name">
+
+    <!-- OG Meta Tags to improve the way the post looks when you share the page on Facebook, Twitter, LinkedIn -->
+    <meta property="og:site_name" content="" /> <!-- website name -->
+    <meta property="og:site" content="" /> <!-- website link -->
+    <meta property="og:title" content="" /> <!-- title shown in the actual shared post -->
+    <meta property="og:description" content="" /> <!-- description shown in the actual shared post -->
+    <meta property="og:image" content="" /> <!-- image link, make sure it's jpg -->
+    <meta property="og:url" content="" /> <!-- where do you want your post to link to -->
+    <meta name="twitter:card" content="summary_large_image"> <!-- to have large image post format in Twitter -->
+
+    <!-- Webpage Title -->
+    <title>OJT Developers</title>
+
+    <!-- Styles -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&family=Poppins:wght@600&display=swap"
+        rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/fontawesome-all.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+
+    <!-- Favicon  -->
+    <link rel="icon" href="app/dist/img/division.png">
+</head>
+
+<body data-spy="scroll" data-target=".fixed-top">
+
+
+
+    <div class="cards-1">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2 class="h2-heading">About the OJT Developers</h2>
+                    <p class="h2-heading testimonial-author">ICT-IMS is developed by the Cor Jesu College students who
+                        partake their On-The-Job Training</p>
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <!-- Card -->
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="details">
+                                <img src="https://scontent.fdvo3-1.fna.fbcdn.net/v/t39.30808-6/300809726_1233584557478883_1080015793595662566_n.jpg?stp=dst-jpg_s1080x2048&_nc_cat=111&cb=99be929b-3346023f&ccb=1-7&_nc_sid=a4a2d7&_nc_eui2=AeHWw9E7pMsB1gjKfd65Un6iypzlx3PPHxjKnOXHc88fGN5txW0OcFo0OPMd-R6F0p8CKTyJ1uBka2JPWIo_SjUM&_nc_ohc=Tnq3mt4OgtoAX_K_WVQ&_nc_ht=scontent.fdvo3-1.fna&oh=00_AfD1mpNTlo20OBfay_BCOJJ8PabXfyCH7RbfQ_QWcDBrdw&oe=64A94E35"
+                                    alt="alternative">
+                                <div class="text">
+                                    <div class="testimonial-author">Geperson C. Mamalias</div>
+                                    <div class="occupation">Bachelor of Science in Information Technology</div>
+                                </div> <!-- end of text -->
+                            </div> <!-- end of testimonial-details -->
+                        </div>
+                    </div>
+                    <!-- end of card -->
+
+                    <!-- Card -->
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="details">
+                                <img src="https://scontent.fdvo3-1.fna.fbcdn.net/v/t1.6435-9/167539728_452406732752396_5731336306418332209_n.jpg?_nc_cat=105&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHVXGxvGiU-SbZwxIlGU290CUUSp9jxIJAJRRKn2PEgkLnIBkI_8OLPanJio3AoyuD2R-E_C6C1U79RJG3ydCsI&_nc_ohc=f9fUvlZxDlwAX_k2vqY&_nc_ht=scontent.fdvo3-1.fna&oh=00_AfCtAfgGB50H9NRrt2C71eG7Rx3jIjRTtv6uCM6GiDsH_w&oe=64CB2935"
+                                    alt="alternative">
+                                <div class="text">
+                                    <div class="testimonial-author">Ivan Roger E. Contorno</div>
+                                    <div class="occupation">Bachelor of Science in Information Technology</div>
+                                </div> <!-- end of text -->
+                            </div> <!-- end of testimonial-details -->
+                        </div>
+                    </div>
+                    <!-- end of card -->
+
+                    <!-- Card -->
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="details">
+                                <img src="https://scontent.fdvo3-1.fna.fbcdn.net/v/t39.30808-6/350484425_1181905092503509_4647151919416718189_n.jpg?_nc_cat=102&cb=99be929b-3346023f&ccb=1-7&_nc_sid=174925&_nc_eui2=AeFRUuqkHI7LRz-YqoB1DQy5KVXB1zaOqjEpVcHXNo6qMfmzx3uOAaLuYDzU3WRi19-dogTuvw4T7MXjfaZpIbdl&_nc_ohc=3nrNRbPnQrcAX83pKui&_nc_ht=scontent.fdvo3-1.fna&oh=00_AfAyfL1mJD-zJLDh69CkbXoLj70dMxMwUF84zNTO_hB3TQ&oe=64A89583"
+                                    alt="alternative">
+                                <div class="text">
+                                    <div class="testimonial-author">Juwe Ian T. Sasuman</div>
+                                    <div class="occupation">Bachelor of Science in Information Technology</div>
+                                </div> <!-- end of text -->
+                            </div> <!-- end of testimonial-details -->
+                        </div>
+                    </div>
+                    <!-- end of card -->
+
+                    <!-- Card -->
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="details">
+                                <img src="https://scontent.fdvo3-1.fna.fbcdn.net/v/t1.6435-9/168306588_120581446766615_1532512993241501165_n.jpg?_nc_cat=108&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFtSSUiwAQ0XvNtD1CCWPaw2Bg-Uj61RG3YGD5SPrVEbbaRKtxfOsIxoW846uZ6yMAWyfwO1mxkzbAYWqIsO-0m&_nc_ohc=XqJ1PFOhRY0AX87YuKs&_nc_ht=scontent.fdvo3-1.fna&oh=00_AfBnVnHWfkehcRyZSyFiWm5RauUrcb10e8gP5Nzn8Zbv6Q&oe=64CB452F"
+                                    alt="alternative">
+                                <div class="text">
+                                    <div class="testimonial-author">Francis Emmanuel P. Decena</div>
+                                    <div class="occupation">Bachelor of Science in Information Technology</div>
+                                </div> <!-- end of text -->
+                            </div> <!-- end of testimonial-details -->
+                        </div>
+                    </div>
+                    <!-- end of card -->
+
+                    <!-- Card -->
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="details">
+                                <img src="https://scontent.fdvo3-1.fna.fbcdn.net/v/t39.30808-6/354223541_967569501251665_7998953369636639552_n.jpg?_nc_cat=102&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFwxGqV6rFsgH_8-eLxOlYB4UvietKca3rhS-J60pxrehgYCa8oJMe86k8GUBET4sAepQ68-xa6Y0_OLlOKNaEO&_nc_ohc=mZ3yDEKZjkMAX-IaRMo&_nc_ht=scontent.fdvo3-1.fna&oh=00_AfD6M5dHKKesPsbXqZxNvxRnub4Hgn3oeRcd7ZqIsNniLQ&oe=64A9514A"
+                                    alt="alternative">
+                                <div class="text">
+                                    <div class="testimonial-author">Alexander Gil M. Samoyan</div>
+                                    <div class="occupation">Bachelor of Science in Information Technology</div>
+                                </div> <!-- end of text -->
+                            </div> <!-- end of testimonial-details -->
+                        </div>
+                    </div>
+                    <!-- end of card -->
+
+                    <!-- Card -->
+
+
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of cards-1 -->
+
+
+
+    <!-- Scripts -->
+    <script src="js/jquery.min.js"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
+    <script src="js/bootstrap.min.js"></script> <!-- Bootstrap framework -->
+    <script src="js/jquery.easing.min.js"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
+    <script src="js/scripts.js"></script> <!-- Custom scripts -->
+</body>
+
+</html>
